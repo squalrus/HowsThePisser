52a7c265b87eed1e6b1224a0595601ca99e3f76f
Josh Holtz <josh@rokkincat.com>
2013-01-27 15:43:20 -0600

    Added search by nearby

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -3,9 +3,14 @@ HowsThePisser.controllers do
 	get "/search" do
 		protected!
 
+		near = params[:near]
+		if near.nil? || near == ""
+			near = "Milwaukee,WI"
+		end
+
 		access_token = OAuth2::AccessToken.new(client, current_user.foursquare_access_token)
 
-		response = access_token.get("https://api.foursquare.com/v2/venues/search?near=Milwaukee,WI&#{v_param}")
+		response = access_token.get("https://api.foursquare.com/v2/venues/search?near=#{near}&#{v_param}")
 		@venues = response['response']['venues']
 
 		render :results
